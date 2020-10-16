bd7f82a403770e498c315ed867250ab5778bcd13
Alexandru-Paul Dumitru <alexandru.dumitru@broadcom.com>
2020-10-16 23:55:54 +0200

    Updates

---
+++ precache-manifest.ab4097e14f41ad19ac872b3408263839.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18d8b92d74fd2f1df0222b0603b166f5",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "e9061903a78537a3998b",
+    "url": "/website/static/css/main.9bad15a9.chunk.css"
+  },
+  {
+    "revision": "ec254b023461f398d9ba",
+    "url": "/website/static/js/2.8619bb59.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/website/static/js/2.8619bb59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9061903a78537a3998b",
+    "url": "/website/static/js/main.22ae729e.chunk.js"
+  },
+  {
+    "revision": "2a9ccbdf38ef80a47b86",
+    "url": "/website/static/js/runtime-main.076ce26e.js"
+  }
+]);
